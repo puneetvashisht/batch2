6cf5b5c9923aa5fad92ee799af4fc8f5dc39c6fb
puneet <puneetvashsiht@gmail.com>
2016-11-19 16:25:11 +0530

    13

--- newangular/build/main.js
+++
@@ -1,7 +0,0 @@
-///<reference path="./../typings/globals/core-js/index.d.ts"/>
-"use strict";
-var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
-var home_module_1 = require('./app/home/home.module');
-platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(home_module_1.HomeModule);
-
-//# sourceMappingURL=main.js.map
